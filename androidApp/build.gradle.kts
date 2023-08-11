54ec241f12f0c0610630066fb502fc5da677bd57
FUZIK <rembozebest@gmail.com>
2023-08-11 18:13:11 +0300

    Version 0.3

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "dev.andrew.prosto"
         minSdk = 21
         targetSdk = 33
-        versionCode = 6
-        versionName = "0.2"
+        versionCode = 7
+        versionName = "0.3"
         // Required when setting minSdkVersion to 20 or lower
         // multiDexEnabled true
 
